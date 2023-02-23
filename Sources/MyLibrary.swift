081fa1d2f91f2fc2bbff15a80df56cf45b6ecdd2
vinodkalpaka <vinod.wst@gmail.com>
2023-02-23 10:31:39 +0530

    feat: feature2

--- Sources/MyLibrary.swift
+++ Sources/MyLibrary.swift
@@ -4,3 +4,9 @@ public func hiThere() -> String {
     // fourth change
     return "Hello Integraa from the library! Rename View"
 }
+public func feature1() -> String {
+    return "feature1"
+}
+public func feature2() -> String {
+    return "feature2"
+}
